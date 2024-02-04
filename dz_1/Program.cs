f9fb8dc2f3e4fdf090b92090c830abdd2a3f2256
Evgeniy <evalrez@mail.ru>
2024-02-04 18:46:02 +0300

    add dz_2

--- dz_1/Program.cs
+++ dz_1/Program.cs
@@ -1,4 +1,9 @@
-﻿string Len(int start, int stop)
+﻿// Задача 1: Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы.
+
+string Len(int start, int stop)
 {
     if (start == stop) return Convert.ToString(start);
     return start + " " + Len(start + 1, stop);
