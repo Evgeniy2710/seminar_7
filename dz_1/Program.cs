3c11a1dd6d2b53669e94eddd7acec9095907e3e9
Evgeniy <evalrez@mail.ru>
2024-02-04 16:18:24 +0300

    add dz_1

---
+++ dz_1/Program.cs
@@ -0,0 +1,13 @@
+﻿string Len(int start, int stop)
+{
+    if (start == stop) return Convert.ToString(start);
+    return start + " " + Len(start + 1, stop);
+}
+
+Console.WriteLine("Введите начальное число:");
+int sta = Convert.ToInt32(Console.ReadLine()!);
+
+Console.WriteLine("Введите конечное число:");
+int sto = Convert.ToInt32(Console.ReadLine()!);
+
+Console.WriteLine(Len(sta, sto));
